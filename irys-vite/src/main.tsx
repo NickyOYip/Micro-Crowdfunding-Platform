4bb30c37e8711e255c0fe9cd42dc85e71d276a5f
NickyOYip <150511100+NickyOYip@users.noreply.github.com>
2025-04-05 21:48:22 +0800

    ui env prepare

--- irys-vite/src/main.tsx
+++ irys-vite/src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import DataProvider from './provider/dataProvider.tsx'
-import WalletProvider from './provider/walletProvider.tsx'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <DataProvider>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
-    </DataProvider>
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Router>
+      <App />
+    </Router>
+  );
--- irys-vite/vite.config.ts
+++ irys-vite/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,14 @@ export default defineConfig({
       protocolImports: true, // Polyfill node: protocol imports
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   resolve: {
     alias: {
       // Polyfill Node.js core modules
